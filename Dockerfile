1408632b56963aeecc31e1aa97c337f66dc0482a
GemmaTuron <gemma@ersilia.io>
2023-04-06 14:11:38 +0200

    model

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM bentoml/model-server:0.11.0-py37
 MAINTAINER ersilia
 
 RUN pip install rdkit
+RUN pip install molvs
+RUN pip install scikit-learn==0.24.2
 
 WORKDIR /repo
 COPY . /repo
--- model/framework/code/main.py
+++ model/framework/code/main.py
@@ -2,8 +2,10 @@
 import os
 import csv
 import sys
+import pickle
 from rdkit import Chem
-from rdkit.Chem.Descriptors import MolWt
+from rdkit.Chem import AllChem, DataStructs
+import numpy as np
 
 # parse arguments
 input_file = sys.argv[1]
@@ -13,9 +15,26 @@ output_file = sys.argv[2]
 root = os.path.dirname(os.path.abspath(__file__))
 
 # my model
-def my_model(smiles_list):
-    return [MolWt(Chem.MolFromSmiles(smi)) for smi in smiles_list]
 
+def mol2fp(mol, radius = 3):
+    fp = AllChem.GetMorganFingerprintAsBitVect(mol, radius = radius)
+    array = np.zeros((1,))
+    DataStructs.ConvertToNumpyArray(fp, array)
+    return array
+
+def compute_ECFP6(smiles):
+    mols = [Chem.MolFromSmiles(i) for i in smiles]
+    arr = np.empty((0,2048), int).astype(int)
+    for i in mols:
+        fp = mol2fp(i)
+        arr = np.vstack((arr, fp))
+    return arr
+
+def build_model(smiles): 
+    input_data=compute_ECFP6(smiles)
+    load_model = pickle.load(open(os.path.join(root,"../..", "checkpoints", 'app.pkl'), 'rb'))
+    prediction = load_model.predict(input_data)
+    return prediction
 
 # read SMILES from .csv file, assuming one column with header
 with open(input_file, "r") as f:
@@ -24,11 +43,11 @@ with open(input_file, "r") as f:
     smiles_list = [r[0] for r in reader]
 
 # run model
-outputs = my_model(smiles_list)
+outputs = build_model(smiles_list)
 
 # write output in a .csv file
 with open(output_file, "w") as f:
     writer = csv.writer(f)
-    writer.writerow(["value"])  # header
+    writer.writerow(["class"])  # header
     for o in outputs:
         writer.writerow([o])
